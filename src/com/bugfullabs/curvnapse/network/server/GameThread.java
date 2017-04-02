6ebcfaa6791c56b30ffd257e0bf0935a247decb0
Wojciech <wojciech@gruszka.eu>
2017-04-02 15:33:59 +0200

    Spawn only selected PowerUps. Random starting position.

--- src/com/bugfullabs/curvnapse/network/server/GameThread.java
+++ src/com/bugfullabs/curvnapse/network/server/GameThread.java
@@ -22,20 +22,21 @@ public class GameThread implements ClientThread.ClientListener {
     private List<ClientThread> mClients;
 
     private boolean mWalls;
-    private int mNextPowerupTime;
+    private int mNextPowerUpTime;
     private Random mRandom;
 
 
     public GameThread(Game pGame, List<ClientThread> pClients) {
         mClients = pClients;
         mGame = pGame;
+        mRandom = new Random();
         mTimer = new Timer();
         mSnakes = new HashMap<>();
-        mGame.getPlayers().forEach(player -> mSnakes.put(player, createNewSnake(player)));
 
         mWalls = false;
-        mRandom = new Random();
-        mNextPowerupTime = mRandom.nextInt(4000) + 500;
+
+        mNextPowerUpTime = mRandom.nextInt(4000) + 500;
+        mGame.getPlayers().forEach(player -> mSnakes.put(player, createNewSnake(player)));
 
         mTimer.scheduleAtFixedRate(new TimerTask() {
             @Override
@@ -43,12 +44,10 @@ public class GameThread implements ClientThread.ClientListener {
                 double delta = 1000 / 60; //FIXME: WCALE NIE
                 LinkedList<SnakeFragment> fragments = new LinkedList<>();
 
-                mNextPowerupTime -= delta;
-                if (mNextPowerupTime < 0) {
-                    mNextPowerupTime = mRandom.nextInt(4000) + 3000;
-                    PowerUp.PowerType type = PowerUp.PowerType.values()[mRandom.nextInt(16)];
-                    Vec2 pos = new Vec2(mRandom.nextInt(mGame.getBoardWidth()), mRandom.nextInt(mGame.getBoardHeight()));
-                    mClients.forEach(client -> client.sendMessage(new SpawnPowerUpMessage(type, pos)));
+                mNextPowerUpTime -= delta;
+                if (mNextPowerUpTime < 0) {
+                    mNextPowerUpTime = mRandom.nextInt(4000) + 3000;
+                    nextPowerUp();
                 }
 
                 mSnakes.forEach((player, snake) -> {
@@ -80,25 +79,45 @@ public class GameThread implements ClientThread.ClientListener {
         mTimer.cancel();
     }
 
+    private void nextPowerUp() {
+        int id;
+
+        do
+            id = mRandom.nextInt(mGame.getPowerUps().length);
+        while (!mGame.getPowerUps()[id]);
+
+        PowerUp.PowerType type = PowerUp.PowerType.values()[id];
+        Vec2 pos = randomPosition();
+        mClients.forEach(client -> client.sendMessage(new SpawnPowerUpMessage(type, pos)));
+    }
+
+    private Vec2 randomPosition() {
+        int margin = 50;
+        int x = mRandom.nextInt(mGame.getBoardWidth() - margin * 2) + margin;
+        int y = mRandom.nextInt(mGame.getBoardHeight() - margin * 2) + margin;
+        return new Vec2(x, y);
+    }
+
     private Snake createNewSnake(Player pPlayer) {
         Random rnd = new Random();
-        //TODO: add margin, avoid conflict
-        int x = rnd.nextInt(mGame.getBoardWidth());
-        int y = rnd.nextInt(mGame.getBoardHeight());
+        //TODO: avoid conflict
         double angle = rnd.nextDouble() * Math.PI;
-        //TODO: TEMPORAL WORKAROUND
-        x = 250;
-        y = 250;
-        angle = 0;
 
-        return new Snake(pPlayer.getID(), new Vec2(x, y), angle, pPlayer.getColor());
+        return new Snake(pPlayer.getID(), randomPosition(), angle, pPlayer.getColor());
     }
 
     @Override
     public void onClientMessage(ClientThread pClientThread, Message pMessage) {
         if (pMessage.getType() == Message.Type.CONTROL_UPDATE) {
             ControlUpdateMessage msg = (ControlUpdateMessage) pMessage;
-            Snake snake = mSnakes.entrySet().stream().filter(e -> e.getKey().getID() == msg.getPlayerID()).findFirst().get().getValue();
+
+            Snake snake = mSnakes.entrySet()
+                    .stream()
+                    .filter(e -> e.getKey().getID() == msg.getPlayerID())
+                    .findFirst()
+                    .get()
+                    .getValue();
+
             if (msg.getAction() == ControlUpdateMessage.Action.UP)
                 snake.turnEnd();
             else if (msg.getDirection() == ControlUpdateMessage.Direction.LEFT)
